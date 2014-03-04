e72e0684bad161d0f126f0a676b86a9c128ab8de
Scott Lystig Fritchie <slfritchie@snookles.com>
2014-03-04 19:14:51 +0900

    More noodling, end of a good exploring day

--- priv/scenario/trigger_commonpaths.erl
+++ priv/scenario/trigger_commonpaths.erl
@@ -41,6 +41,7 @@ static char *interesting_strings[] = {
 };
 
 static int flock_op_array[] = { LOCK_SH, LOCK_EX };
+static int fcntl_cmd_array[] = { F_SETLK, F_SETLKW };
 static int open_write_op_array[] = { O_WRONLY, O_RDWR };
 #endif
 ",
@@ -97,12 +98,42 @@ typedef struct {
     }
 ",
 
+    %% Memo: IntArgC_ means "integer arg checking"
+    IntArgC_CHeaders = ["<sys/fcntl.h>"],
+    IntArgC_TStruct = "
+typedef struct {
+    char *i_name;  /* Must be present */
+    int array[11]; /* Array of ints to check, 0 signals end of array */
+}",
+    IntArgC_TNewInstanceArgs = "int *array",
+    IntArgC_TNewInstanceBody = "
+    int i;
+
+    for (i = 0; array[i] != 0 && i < sizeof(a->array); i++) {
+        a->array[i] = array[i];
+    }
+    array[i] = 0;
+",
+    %% NOTE: 'path' below matches last arg to config()
+    IntArgC_TBody = "
+    int i;
+
+    res = 0;
+    for (i = 0; a->array[i] != 0; i++) {
+        if (a->array[i] == cmd) {
+            res++;
+            break;
+        }
+    }
+",
+
     [trigger_args_of_intercept:config(
          PathC_CHeaders, [DoOnlyOnceHack], [],
          true, true, false,
          PathC_TStruct, "", PathC_TNewInstanceArgs, PathC_TNewInstanceBody,
          InterceptName, PathC_TBody,
-         InterceptName, "path") || InterceptName <- ["access", "open",
+         InterceptName, "path") || InterceptName <- ["access", "stat",
+                                                     "open",
                                                      "unlink", "unlinkat"]]
     ++
     [trigger_args_of_intercept:config(
@@ -113,11 +144,11 @@ typedef struct {
          InterceptName, "bits") || InterceptName <- ["flock"]]
     ++
     [trigger_args_of_intercept:config(
-         BitC_CHeaders, [], [],
+         IntArgC_CHeaders, [], [],
          true, true, false,
-         BitC_TStruct, "", BitC_TNewInstanceArgs, BitC_TNewInstanceBody,
-         InterceptName, BitC_TBody,
-         InterceptName, "bits") || InterceptName <- []]
+         IntArgC_TStruct, "", IntArgC_TNewInstanceArgs, IntArgC_TNewInstanceBody,
+         InterceptName, IntArgC_TBody,
+         InterceptName, "cmd") || InterceptName <- ["fcntl"]]
     ++
     [trigger_random:config()]
     ++
@@ -125,13 +156,16 @@ typedef struct {
      %% A quick glance at the code suggests that the LevelDB code never
      %% checks the return status of a variable.  {shrug}
 
+     %% HRM, seems like none of the EUnit tests trigger calls to access()
+     %% by the leveldb code.  There are plenty of calls by the VM code
+     %% loader and by the eqc framework, though.
      #fi{	% both?/OS X version
          name = "access",
          type = intercept,
          intercept_args = "const char *path, int amode",
          intercept_args_call = "path, amode",
          c_headers = ["<unistd.h>"],
-         intercept_errno = "ELOOP",
+         intercept_errno = "EFAULT",
          intercept_return_type = "int",
          intercept_return_value = "-1",
          %% Use 2-tuple version here, have the instance name auto-generated
@@ -139,18 +173,48 @@ typedef struct {
                                {"random", "always", "100"}]
      },
      #fi{	% both?/OS X version
+         name = "stat",
+         type = intercept,
+         intercept_args = "const char *path, struct stat *buf",
+         intercept_args_call = "path, buf",
+         c_headers = ["<sys/stat.h>"],
+         intercept_errno = "EFAULT",
+         intercept_return_type = "int",
+         intercept_return_value = "-1",
+         %% Use 2-tuple version here, have the instance name auto-generated
+         intercept_triggers = [{"i_arg_stat_path", "\"-unused-arg-\""},
+                               {"random", "always", "25"}]
+     },
+     #fi{	% both?/OS X version
          name = "flock",
          type = intercept,
          intercept_args = "int fd, int checked_operation",
          intercept_args_call = "fd, checked_operation",
          c_headers = ["<sys/file.h>"],
-         intercept_errno = "EBADF",
+         intercept_errno = "EDEADLK",
          intercept_return_type = "int",
          intercept_return_value = "-1",
          %% Use 2-tuple version here, have the instance name auto-generated
          intercept_triggers = [{"random", "always", "100"},
                                {"i_arg_flock_bits", "flock_op_array"},
-                               {"random", "", "80"}]
+                               {"random", "", "0"}]
+     },
+     #fi{	% both?/OS X version
+         name = "fcntl",
+         type = intercept,
+         intercept_args = "int filedes, int cmd, ...",
+         intercept_args_call = "filedes, cmd, arg",
+         intercept_body_setup = "    int arg; va_list ap;\n\n" ++
+                                "    va_start(ap, cmd); " ++
+                                "arg = va_arg(ap, int); va_end(ap);\n",
+         c_headers = ["<sys/file.h>"],
+         intercept_errno = "EDEADLK",
+         intercept_return_type = "int",
+         intercept_return_value = "-1",
+         %% Use 2-tuple version here, have the instance name auto-generated
+         intercept_triggers = [{"random", "always", "100"},
+                               {"i_arg_fcntl_cmd", "fcntl_cmd_array"},
+                               {"random", "", "0"}]
      },
      #fi{	% both?/OS X version
          name = "open",
@@ -161,28 +225,55 @@ typedef struct {
                                 "    va_start(ap, checked_operation); " ++
                                 "mode = va_arg(ap, int); va_end(ap);\n",
          c_headers = ["<fcntl.h>", "<stdarg.h>"],
-         intercept_errno = "EDQUOT",
+         intercept_errno = "ENOSPC",
          intercept_return_type = "int",
          intercept_return_value = "-1",
          %% Use 2-tuple version here, have the instance name auto-generated
          intercept_triggers = [{"random", "always", "100"},
                                {"i_arg_open_path", "\"-unused-arg-\""},
-                               {"random", "", "50"}]
+                               {"random", "", "0"}]
      },
-     %% #fi{	% both?/OS X version
-     %%     name = "open",
-     %%     type = intercept,
-     %%     intercept_args = "const char *path, int checked_operation, ...",
-     %%     intercept_args_call = "path, checked_operation",
-     %%     c_headers = ["<fcntl.h>"],
-     %%     intercept_errno = "ENOSPC",
-     %%     intercept_return_type = "int",
-     %%     intercept_return_value = "-1",
-     %%     %% Use 2-tuple version here, have the instance name auto-generated
-     %%     intercept_triggers = [{"random", "always", "100"},
-     %%                           {"i_arg_open_bits", "open_write_op_array"},
-     %%                           {"random", "", "50"}]
-     %% },
+     #fi{	% OS X version
+         name = "opendir$INODE64",
+         type = intercept,
+         intercept_args = "const char *path",
+         intercept_args_call = "path",
+         c_headers = ["<dirent.h>"],
+         intercept_errno = "ENOSPC",
+         intercept_return_type = "DIR *",
+         intercept_return_value = "NULL",
+         %% Use 2-tuple version here, have the instance name auto-generated
+         intercept_triggers = [{"random", "", "0"}]
+     },
+     %% Yup, ftruncate triggers the same bug during open that others do.
+     #fi{	% both?/OS X version
+         name = "ftruncate",
+         type = intercept,
+         intercept_args = "int fd, off_t length",
+         intercept_args_call = "fd, length",
+         c_headers = ["<unistd.h>"],
+         intercept_errno = "ENOSPC",
+         intercept_return_type = "int",
+         intercept_return_value = "-1",
+         %% Use 2-tuple version here, have the instance name auto-generated
+         intercept_triggers = [{"random", "", "0"}]
+     },
+     %% HRM, pread() is heavily used by EQC via dets, bummer.
+     %% We need more smarts here, somehow.
+     %% If nbyte > 9000, then we seem to avoid most/all of dets 8KB
+     %% ?CHUNK_SIZE ops?
+     #fi{	% both?/OS X version
+         name = "pread",
+         type = intercept,
+         intercept_args = "int fd, void *buf, size_t nbyte, off_t offset",
+         intercept_args_call = "fd, buf, nbyte, offset",
+         c_headers = ["<unistd.h>"],
+         intercept_errno = "EBADF",
+         intercept_return_type = "ssize_t",
+         intercept_return_value = "-1",
+         %% Use 2-tuple version here, have the instance name auto-generated
+         intercept_triggers = [{"random", "", "25"}]
+     },
      #fi{	% OS X version
          name = "unlink",
          type = intercept,
